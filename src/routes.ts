3c251bd5fed7257565a30647ed818f83fcf1bca8
igoreor <igoreor24@gmail.com>
2024-10-08 02:24:13 -0300

    feat: criar usuario

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router, Request, Response} from "express";
+import { CreateUserController } from "./controllers/user/CreateUserController";
 
 const router = Router();
 router.get("/test", (request: Request, response: Response) => {
     return response.json({ ok: true});
 
 });
+//users routes
+router.post('/user', new CreateUserController().handle);
 
 export { router };
+    
